6c8e75fcb2a982f6a6bb65b06ed7389a072ec5e0
Tracy Sonyah <sonyahtracy@gmail.com>
2024-06-25 14:02:59 +0000

    done

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const pomodoroBtn = document.getElementById("pomodoro");
 const shortBreak = document.getElementById("short-break");
 const longBreak = document.getElementById("long-break");
 const timer = document.querySelector(".timer p");
@@ -5,26 +6,40 @@ const startTimer = document.getElementById("start");
 const pauseTimer = document.getElementById("pause");
 const restartTimer = document.getElementById("restart");
 
-
+let initialTime;
 let timeLeft;
 let interval;
 
+
+pomodoroBtn.addEventListener("click", () => {
+    timer.innerText = "25:00";
+    timeLeft = 25 * 60;
+    initialTime = timeLeft;
+    console.log("grace!");
+})
+
 longBreak.addEventListener("click", () => {
-    console.log("yes!");
     timer.innerText = "15:00";
     timeLeft = 15 * 60;
+    initialTime = timeLeft
     console.log(timeLeft);
 });
 
 shortBreak.addEventListener("click", () => {
     timer.innerText = "5:00";
     timeLeft = 5 * 60;
+    initialTime = timeLeft;
     console.log(timeLeft);
 });
 
-// restartTimer.addEventListener("click", () => {
-
-// })
+restartTimer.addEventListener("click", () => {
+    clearInterval(interval);
+    timeLeft = initialTime;
+    const minutes = Math.floor(timeLeft / 60);
+    const seconds = timeLeft % 60;
+    timer.innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    console.log("yes!");
+})
 
 startTimer.addEventListener("click", () => {
     clearInterval(interval);
@@ -33,16 +48,18 @@ startTimer.addEventListener("click", () => {
 
 function startCount() {
     if(timeLeft > 0) {
-        const minutes = Math.floor(timeLeft / 60);
-        console.log(minutes);
-        const seconds = timeLeft % 60;
-        console.log(seconds);
-        timer.innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
         timeLeft--;
+        const minutes = Math.floor(timeLeft / 60);
+        const seconds = timeLeft % 60;
+        timer.innerText = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
     }
     else {
         console.log("no!");
         clearInterval(interval);
     }
 }
+
+pauseTimer.addEventListener("click", () => {
+    clearInterval(interval);
+})
 
